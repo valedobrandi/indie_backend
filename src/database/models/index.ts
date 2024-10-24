8aa91cd3325490292cc1b29eae9f4b7205ee2584
valedobrandi <valedobrandi@gmail.com>
2024-10-24 22:57:24 +0200

    posgres heroku config

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from 'sequelize';
 import * as config from '../config/database';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || '')
+const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+})
 
 export default sequelize;
