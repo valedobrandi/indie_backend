3e3d9b719b06ab4410b8c1a7e60fa29902cc6ffb
valedobrandi <valedobrandi@gmail.com>
2024-10-24 22:34:01 +0200

    posgres heroku config

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,11 +1,11 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  host: process.env.MYSQLHOST || 'PostgresCont',
-  port: Number(process.env.MYSQLPORT) || 5432,
-  database: process.env.MYSQLDATABASE || 'subscribers',
-  username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '123456',
+  host: process.env.POSTGRESHOST || 'PostgresCont',
+  port: Number(process.env.POSTGRESPORT) || 5432,
+  database: process.env.POSTGRESDATABASE || 'subscribers',
+  username: process.env.POSTGRESUSER || 'root',
+  password: process.env.POSTGRESPASSWORD || '123456',
   dialect: 'postgres',
 }
 
--- src/database/seeders/02-email.ts
+++ src/database/seeders/02-email.ts
@@ -3,16 +3,7 @@ import { QueryInterface } from 'sequelize';
 export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('emails', [
-      { email: 'user1@example.com', createdAt: new Date() },
-      { email: 'user2@example.com', createdAt: new Date() },
-      { email: 'user3@example.com', createdAt: new Date() },
-      { email: 'user4@example.com', createdAt: new Date() },
-      { email: 'user5@example.com', createdAt: new Date() },
-      { email: 'user6@example.com', createdAt: new Date() },
-      { email: 'user7@example.com', createdAt: new Date() },
-      { email: 'user8@example.com', createdAt: new Date() },
-      { email: 'user9@example.com', createdAt: new Date() },
-      { email: 'user10@example.com', createdAt: new Date() },
+      { email: 'start@seed.com', createdAt: new Date() },
     ], {});
   },
 
