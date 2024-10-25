37c22b5f9dcecacf892176c5fd8bb02a60d625bf
valedobrandi <valedobrandi@gmail.com>
2024-10-25 22:43:13 +0200

    sequelize config

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,12 +1,27 @@
 import { Options } from 'sequelize';
+import { URL } from 'url'
+
+const uri = process.env.DATABASE_URL || ''
+
+const parsedUrl = new URL(uri)
+
+const host = parsedUrl.hostname;
+const database = parsedUrl.pathname.split('/')[1];
+const user = parsedUrl.username;
+const port = parsedUrl.port;
+const password = parsedUrl.password;
 
 const config: Options = {
-  host: process.env.POSTGRESHOST || 'PostgresCont',
-  port: Number(process.env.POSTGRESPORT) || 5432,
-  database: process.env.POSTGRESDATABASE || 'subscribers',
-  username: process.env.POSTGRESUSER || 'root',
-  password: process.env.POSTGRESPASSWORD || '123456',
+  host: host || 'PostgresCont',
+  port: Number(port) || 5432,
+  username: user || 'root',
+  database: database || 'subscribers',
+  password: password || '123456',
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: true,
+    rejectUnauthorized: false
+  }
 }
 
 export = config;
