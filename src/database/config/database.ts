dcc0ef4676c7b2bde1350294a42d94744976d397
valedobrandi <valedobrandi@gmail.com>
2024-11-01 11:01:49 +0100

    filter questions

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,7 +1,7 @@
 import { Options } from 'sequelize';
 import { URL } from 'url'
 
-const uri = process.env.DATABASE_URL || ''
+const uri = process.env.DATABASE_URL || 'postgres://root:123456@postgres-heroku:5432/subscribers'
 
 const parsedUrl = new URL(uri)
 
@@ -18,7 +18,10 @@ const config: Options = {
   database: database || 'subscribers',
   password: password || '123456',
   dialect: 'postgres',
-  dialectOptions: {
+}
+
+if (process.env.DATABASE_URL) {
+  config.dialectOptions = {
     ssl: {
     rejectUnauthorized: false
   }}
--- src/services/quiz.service.ts
+++ src/services/quiz.service.ts
@@ -8,21 +8,25 @@ export default class QuizService {
     const response = await fetch(http.quizapi);
 
     const data = await response.json();
-        
-    const questions = data.map((question: IQuestion) => {
-      const getCorrectAnswers = Object.entries(question.correct_answers).find(
+    
+    const filterMultipleChoice = data.filter((question: IQuestion) => {
+      return question.multiple_correct_answers === "false"
+    })
+    
+    const questions = filterMultipleChoice.map((question: IQuestion) => {
+      const [getCorrectAnswers] = Object.entries(question.correct_answers).find(
         ([_key, value]) => value === "true") || ["", ""];
-
+        
       const getIndexCorrectAnswer = Object.keys(
         question.correct_answers
-      ).indexOf(getCorrectAnswers[0]);
+      ).indexOf(getCorrectAnswers);
       
       return {
         ...question,
         correct_answers: getIndexCorrectAnswer,
       };
     });
-
+    
     const filteredQuestions = questions.filter(
       ({ correct_answers }: { correct_answers: number }) =>
         correct_answers !== -1
